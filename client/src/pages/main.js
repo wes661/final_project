e0d6581563ab413a167088337a517daa41998600
Corey Richard <ullrtheglorious@gmail.com>
2018-06-19 21:03:23 -0700

    changed extends rather than arrow, scss and css setup

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -3,20 +3,27 @@ import logo from "../logo.svg";
 import "../css/App.css";
 import { Link } from "react-router-dom";
 
-const Main = () => (
-<div className="App">
+class Main extends React.Component {
+    componentDidMount() {
+    // ------    put on page load js here ------ //
+    }
+    render() {
+        return (
+    // -------- Start HTML here -------- //
+    <div className="App">
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
+        </header>
+        <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    <p>
-        <Link to="/homepage"> homepage </Link>
-    </p>  
-
-</div>
-);
+        </p>
+        <p>
+            <Link to="/homepage"> homepage </Link>
+        </p>  
+    </div>
+    // ------ End HTML here -------------- //
+    )}
+};
 
 export default Main;
