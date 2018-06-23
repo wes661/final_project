9074e4e73c7eef6b172864b4d0a22cf50e77f3b9
geina corrente <geina_marie@yahoo.com>
2018-06-23 12:34:25 -0700

    fixes to medi page

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "../css/login.css";
 
 
@@ -11,20 +11,40 @@ class Main extends React.Component {
         return (
     // -------- Start HTML here -------- //
     <div className="wrapper">
-    <div className="col-6">
+      <div className="container">
+        <div className="col-md-6">
+          <div className="logbox">
+            <form className="signup" method="post" action="/signup">
+              <h1>create an account</h1>
+              <input name="user[name]" type="text" placeholder="What's your username?" pattern="^[\w]{3,16}$" autofocus="autofocus" required="required" className="input pass"/>
+              <input name="user[password]" type="password" placeholder="Choose a password" required="required" className="input pass"/>
+              <input name="user[password2]" type="password" placeholder="Confirm password" required="required" className="input pass"/>
+              <input name="user[email]" type="email" placeholder="Email address" className="input pass"/>
+              <input type="submit" value="Sign up!" className="inputButton"/>
+              <div className="text-center">
+                  already have an account? <a href="" className="login_id">login</a>
+              </div>
+            </form>
+          </div>
+      </div>
+    {/* <!--col-md-6--> */}
+    
+   <div className="col-md-6">
     <div className="logbox">
-      <form className="signup" method="post" action="/signup">
-        <h1>Account login</h1>
-        <input name="user[email]" type="email" placeholder="enter your email" className="input pass" />
-        <input name="user[password]" type="password" placeholder="enter your password" required="required" className="input pass" />
-        <input type="submit" value="Sign in" className="inputButton" />
-        <div className="text-center" >
-          <a href="# " className=" ">create an account</a> - <a href="# " className=" ">forgot password</a>
-        </div>  
-      </form>
+        <form className="signup" method="post" action="/signup">
+          <h1>account login</h1>
+          <input name="user[email]" type="email" placeholder="enter your email" className="input pass"/>
+          <input name="user[password]" type="password" placeholder="enter your password" required="required" className="input pass"/>
+          <input type="submit" value="Sign in!" className="inputButton"/>
+          <div className="text-center">
+            <a href="" >create an account</a> - <a href="" >forgot password</a>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-  </div>
+    </div>
+    
     // ------ End HTML here -------------- //
     )}
 };
--- client/src/pages/medications.js
+++ client/src/pages/medications.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/medications.css";
+import "../css/medication.css";
 import { Link } from "react-router-dom";
 
 class Medications extends React.Component {
@@ -10,51 +10,59 @@ class Medications extends React.Component {
         return (
     // -------- Start HTML here -------- //
     <div className="wrapper">
-        <h1>Medication page</h1>
-        <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-        </p>
-        <p>
-            <Link to="/homepage"> homepage </Link>
-        </p> 
+        <div className="container-fluid stickyHeader">
+            <div className="page-header">
+                <h1 className="minitron">
+                  Medication
+                </h1>
+                <p>
+                  <Link to="/homepage"> Homepage </Link>
+                </p> 
+            </div>
+        </div>
+        
 
    
     <table className="table">
-        <thead>
+        <thead className="topBand">
           <tr>
-            <th>#</th>
             <th>Pill</th>
             <th>Times taken</th>
+            <button type="button" className="btn btn-light btn-sm addBtn" data-toggle="modal" data-target="#exampleModal">
+              Add
+            </button> 
+            {/* <td><span class="badge badge-pill badge-primary">Add</span></td> */}
+           
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
+            {/* <th scope="row">1</th> */}
             <td>Xanax</td>
             <td>Evening</td>
+            <td><span className="badge badge-pill badge-primary">Done</span></td>
           </tr>
           <tr>
-            <th scope="row">2</th>
+            {/* <th scope="row">2</th> */}
             <td>Percocet</td>
             <td>Morning and Evening</td>
+            <td><span className="badge badge-pill badge-primary">Done</span></td>
           </tr>
         </tbody>
     </table>
-    //  Button trigger modal 
+
+    
+    {/* //  Button trigger modal 
     <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
       Launch demo modal
-    </button>
+    </button> */}
 
-    // Modal 
-    <div className="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    {/* Modal  */}
+    <div className="modal exampleModal"  tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 className="modal-title exampleModalLabel">Modal title</h5>
             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -69,11 +77,11 @@ class Medications extends React.Component {
         </div>
       </div>
     </div>
-    /* <div>
+    <div>
     <ul className="list-group">
-        <li className="list-group-item active">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
+        <li className="list-group-item active">Past Scripts</li>
+        <li className="list-group-item">Viccodin</li>
+        <li className="list-group-item">Methaqualone</li>
     </ul>
     </div> 
     </div>
