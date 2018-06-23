2ffd63b45f3ef7cc2d52076bd31b855b65b606e1
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-06-23 13:58:04 -0700

    Added form to modal inputs, next save to calendar

--- client/src/components/CalendarLayout/Modal.jsx
+++ client/src/components/CalendarLayout/Modal.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import Modal from 'react-responsive-modal';
+import Form from './ModalForm';
 import './custom-animation.css';
 
 export default class AppModal extends React.Component {
     render() {
         return (
-            <div className="example">
-                <Modal
-                    open={this.props.open}
-                    onClose={this.props.onCloseModal}
-                    center
-                    classNames={{
-                        transitionEnter: 'transition-enter',
-                        transitionEnterActive: 'transition-enter-active',
-                        transitionExit: 'transition-exit-active',
-                        transitionExitActive: 'transition-exit-active',
-                    }}
-                    animationDuration={1000}
-                >
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                        pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                        hendrerit risus, sed porttitor quam.
-          </p>
-                </Modal>
-            </div>
+            <Modal
+                open={this.props.open}
+                onClose={this.props.onCloseModal}
+                center
+                classNames={{
+                    transitionEnter: 'transition-enter',
+                    transitionEnterActive: 'transition-enter-active',
+                    transitionExit: 'transition-exit-active',
+                    transitionExitActive: 'transition-exit-active',
+                }}
+
+                animationDuration={1000}
+
+            >
+
+                <Form />
+            </Modal>
+
         );
     }
 }
--- client/src/pages/appointments.js
+++ client/src/pages/appointments.js
@@ -8,44 +8,55 @@ class Appointments extends React.Component {
     constructor(props) {
         super();
         this.state = {
-          date: new Date(),
-          open: false
+            date: new Date(),
+            open: false,
+            events: [
+                {
+                    id: 1,
+                    date: 2,
+                    year: 2018,
+                    month: 2,
+                    title: "Holy"
+                }
+            ]
         }
-      }
-      onOpenModal = () => {
+    }
+    onOpenModal = () => {
         this.setState({ open: true });
-      };
-    
-      onCloseModal = () => {
+    };
+
+    onCloseModal = () => {
         this.setState({ open: false });
-      };
-    
-      onDateChange = date => this.setState({ date });
-    
+    };
+
+    onDateChange = date => this.setState({ date });
+
+
     componentDidMount() {
-    // ------    put on page load js here ------ //
+        // ------    put on page load js here ------ //
     }
     render() {
         return (
-    // -------- Start HTML here -------- //
-    <div>
-        <h1>appointment page</h1>
-        <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
+            // -------- Start HTML here -------- //
+            <div>
+                <h1>appointment page</h1>
+                <p>
+                    Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
+              ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
+              quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+              imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
+              diam, sit amet facilisis lectus blandit at.
     </p>
-        <CalendarLayout date={this.state.date} onOpenModal={this.onOpenModal} onDateChange={this.onDateChange} />
-        <AppModal open={this.state.open} onCloseModal={this.onCloseModal} />
-     
-        <p>
-            <Link to="/homepage"> homepage </Link>
-        </p> 
-    </div>
-    // ------ End HTML here -------------- //
-    )}
+                <CalendarLayout date={this.state.date} onOpenModal={this.onOpenModal} onDateChange={this.onDateChange} />
+                <AppModal open={this.state.open} onCloseModal={this.onCloseModal} />
+
+                <p>
+                    <Link to="/homepage"> homepage </Link>
+                </p>
+            </div>
+            // ------ End HTML here -------------- //
+        )
+    }
 };
 
 export default Appointments;
