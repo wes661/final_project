1eacddd999af6837a083866c294bd2435d18be88
Wesley Hanson <wrh1600@gmail.com>
2018-06-25 15:43:24 -0700

    added map function to medication page

--- client/src/pages/medications.js
+++ client/src/pages/medications.js
@@ -11,19 +11,15 @@ class Medications extends React.Component {
     }
   }
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const medList = user.meds.map(med => <li>{med.name}</li>);
+
     return (
       // -------- Start HTML here -------- //
       <div className="wrapper">
         <h1>Medication page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus
-          cursus in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit
-          at.
-        </p>
+        <ul>{medList}</ul>
         <p>
           <Link to="/homepage"> homepage </Link>
         </p>
