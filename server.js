9c11c59237a5d041678c0128c1ab467f461c38b4
Wesley Hanson <wrh1600@gmail.com>
2018-07-10 19:13:19 -0700

    Production stuff

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const env = require("dotenv").config();
 
 const users = require("./routes/api/users");
 
